075a1d52bfd47d8ac479eeb3352422267f902160
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-01-12 16:37:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ interval = st.selectbox(
      ('1m', '2m', '5m','15m','30m','60m','90m','1h','1d','5d','1wk','1mo','3mo'))
 
 chartPeriod = st.selectbox(
-     'What candlestick inverval?',
+     'How many days shoud the test last?',
      ('1h','2h','12h','1d','2d', '5d','15d','30d','60d','100d','200d','300d','400d','600d','800d'))
 
 rsiPeriod = int(st.text_input("-Input Desired Rsi Period"))
