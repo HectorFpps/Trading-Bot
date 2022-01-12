b25acdd0cb64eced8f30f8b630f47597586d8d25
dberweger2017 <94843489+dberweger2017@users.noreply.github.com>
2022-01-12 12:04:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ st.line_chart(price)
 st.write("#### -Trading Bot Portfolio Overtime")
 st.line_chart(balance_track)
 
-st.write("### The final balance is " + str(round(balance)))
-st.write("### That is " +str(round(balance-initialBalance)) + "$ in " + chartPeriod + " (" + str(round((balance-initialBalance)/initialBalance*100,1)) + "%). This ticker did " + str(round((price[-1]-price[0])/price[0]*100,1)) + "% in that time.")
+st.write("##### The final balance is " + str(round(balance)))
+st.write("##### That is " +str(round(balance-initialBalance)) + "$ in " + chartPeriod + " (" + str(round((balance-initialBalance)/initialBalance*100,1)) + "%). This ticker did " + str(round((price[-1]-price[0])/price[0]*100,1)) + "% in that time.")
